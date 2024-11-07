604ac909f0472114c0ad600d4ea19660830136fb
murtadho <id.alimurtadho@gmail.com>
2024-11-07 19:35:05 +0700

    revert api key

--- watson_ai/demo_wml_api.streamlit.py
+++ watson_ai/demo_wml_api.streamlit.py
@@ -30,12 +30,12 @@ from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
 # can be stored in an .env or a properties file
 
 # URL of the hosted LLMs is hardcoded because at this time all LLMs share the same endpoint
-url = "https://jp-tok.ml.cloud.ibm.com"
+url = ""
 
 # These global variables will be updated in get_credentials() functions
-watsonx_project_id = "9b88dfc7-95e7-46fd-88c0-8ec180bcd78a"
+watsonx_project_id = ""
 # Replace with your IBM Cloud key
-api_key = "n-cMgsIkVQ7AXpriiKpJKaWaf57H28U-hTE2bePoWC8g"
+api_key = ""
 
 def get_credentials():
 
